2e6680326c4c194fbd91b1c264abbb09acd8126a
Isack Ramirez <isack.ramirez1@gmail.com>
2021-07-16 17:10:00 -0500

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
 
 
 
+var inputText = $("#searchBar");
 
- $.ajax({
-    
-    url: "https:api.openweathermap.org/data/2.5/weather?q=SAN%20ANTONIO&appid=db2bc8e5baccdef1863d3f9f469cc4b2",
-    dataType: 'application/json',
-    complete: function(data){
-        console.log(data.responseText)
-        console.log(data.status);// S1000
-        console.log(data.description);// Success
-        
-        
-    },
-    success: function(data){
-        alert(data)
-    }
-})
 
 
 $('#searchButton').click(function(e){
     e.preventDefault();
-    // Code goes here
-
-    console.log("yay")
+   
+    $.ajax({
+        
+        type: "GET",
+        url: "https:api.openweathermap.org/data/2.5/weather?q=SAN%20ANTONIO&appid=db2bc8e5baccdef1863d3f9f469cc4b2",
+        dataType: 'jsonp',
+        complete: function(data){
+            weatherData=data.responseJSON;
+            console.log(weatherData)
+            console.log(inputText.val);
+           
+            
+        
+            
+        },
+       
+           
+        
+    })
+ 
  });
 
 
